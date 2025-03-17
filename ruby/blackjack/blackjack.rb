05e86139d60e1bce894ebacd4ed5665209024bc3
GoCoder <gocoder7@gmail.com>
2025-03-17 19:49:54 +0900

    ruby: done: blackjack

--- ruby/blackjack/blackjack.rb
+++ ruby/blackjack/blackjack.rb
@@ -1,13 +1,37 @@
 module Blackjack
   def self.parse_card(card)
-    raise "Please implement the Blackjack.parse_card method"
+    case card
+    when 'ace' then 11
+    when 'two' then 2
+    when 'three' then 3
+    when 'four' then 4
+    when 'five' then 5
+    when 'six' then 6
+    when 'seven' then 7
+    when 'eight' then 8
+    when 'nine' then 9
+    else %w[ten jack queen king].include?(card) ? 10 : 0
+    end
   end
 
   def self.card_range(card1, card2)
-    raise "Please implement the Blackjack.card_range method"
+    val = parse_card(card1) + parse_card(card2)
+    case val
+    when 4..11 then 'low'
+    when 12..16 then 'mid'
+    when 17..20 then 'high'
+    when 21 then 'blackjack'
+    end
   end
 
   def self.first_turn(card1, card2, dealer_card)
-    raise "Please implement the Blackjack.first_turn method"
+    range_str = card_range(card1, card2)
+    case range_str
+    when nil then 'P'
+    when 'blackjack' then !%w[ten ace].include?(dealer_card) ? 'W' : 'S'
+    when 'high' then 'S'
+    when 'mid' then parse_card(dealer_card) >= 7 ? 'H' : 'S'
+    else 'H'
+    end
   end
 end
