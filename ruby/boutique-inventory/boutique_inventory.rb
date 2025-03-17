7c8bcf0c34d3f84a149c91ef8588b04ef903a1e7
GoCoder <gocoder7@gmail.com>
2025-03-17 20:47:20 +0900

    ruby: done: boutique-inventory

--- ruby/boutique-inventory/boutique_inventory.rb
+++ ruby/boutique-inventory/boutique_inventory.rb
@@ -4,25 +4,36 @@ class BoutiqueInventory
   end
 
   def item_names
-    raise 'Implement the BoutiqueInventory#item_names method'
+    @items.map do |item|
+      item[:name]
+    end.sort
   end
 
   def cheap
-    raise 'Implement the BoutiqueInventory#cheap method'
+    @items.select do |item|
+      item[:price] < 30
+    end
   end
 
   def out_of_stock
-    raise 'Implement the BoutiqueInventory#out_of_stock method'
+    @items.select do |item|
+      item[:quantity_by_size].empty?
+    end
   end
 
   def stock_for_item(name)
-    raise 'Implement the BoutiqueInventory#stock_for_item method'
+    @items.find do |item|
+      item[:name] == name
+    end[:quantity_by_size]
   end
 
   def total_stock
-    raise 'Implement the BoutiqueInventory#total_stock method'
+    @items.map do |item|
+      item[:quantity_by_size].values.sum
+    end.sum
   end
 
   private
+
   attr_reader :items
 end
